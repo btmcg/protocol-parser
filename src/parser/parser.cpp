7ec1e27b72b831830ffbf4030b4275891b4d9dc8
btmcg <btmcg@eolas>
2020-04-26 17:30:48 -0400

    [parser] print errors to stderr

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -21,27 +21,27 @@ namespace { // unnamed
     {
         int const fd = ::open(filepath.c_str(), O_RDONLY);
         if (fd == -1) {
-            fmt::print("ERROR: open ({}): [{}]\n", filepath.c_str(), std::strerror(errno));
+            fmt::print(stderr, "ERROR: open ({}): [{}]\n", filepath.c_str(), std::strerror(errno));
             return {nullptr, 0};
         }
 
         off_t const file_size = ::lseek(fd, 0, SEEK_END);
         if (file_size == -1) {
-            fmt::print("ERROR: lseek [{}]\n", std::strerror(errno));
+            fmt::print(stderr, "ERROR: lseek [{}]\n", std::strerror(errno));
             ::close(fd);
             return {nullptr, 0};
         }
 
         void* f_ptr = ::mmap(nullptr, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
         if (f_ptr == MAP_FAILED) {
-            fmt::print("ERROR: mmap [{}]\n", std::strerror(errno));
+            fmt::print(stderr, "ERROR: mmap [{}]\n", std::strerror(errno));
             ::close(fd);
             return {nullptr, 0};
         }
 
         if (::close(fd) == -1) {
             // not a critical failure
-            fmt::print("ERROR: close [{}]\n", std::strerror(errno));
+            fmt::print(stderr, "ERROR: close [{}]\n", std::strerror(errno));
         }
 
         return std::make_tuple(f_ptr, file_size);
@@ -53,6 +53,7 @@ parser::parser(std::filesystem::path const& input_file)
         : input_file_(input_file)
         , f_ptr_(nullptr)
         , file_size_(0)
+        , stats_()
 {
     std::tie(f_ptr_, file_size_) = mmap_file(input_file_);
     if (f_ptr_ == nullptr)
@@ -62,7 +63,7 @@ parser::parser(std::filesystem::path const& input_file)
 parser::~parser() noexcept
 {
     if (::munmap(f_ptr_, file_size_) == -1)
-        fmt::print("ERROR: munmap [{}]\n", std::strerror(errno));
+        fmt::print(stderr, "ERROR: munmap [{}]\n", std::strerror(errno));
 }
 
 bool
@@ -94,7 +95,7 @@ parser::parse_gz() noexcept
     zstrm.next_in = nullptr;
 
     if (int const rv = ::inflateInit2(&zstrm, 32); rv != Z_OK) {
-        fmt::print("ERROR: inflateInit returned error {}\n", rv);
+        fmt::print(stderr, "ERROR: inflateInit returned error {}\n", rv);
         return false;
     }
 
@@ -113,16 +114,16 @@ parser::parse_gz() noexcept
         ret = ::inflate(&zstrm, Z_NO_FLUSH);
         switch (ret) {
             case Z_STREAM_ERROR:
-                fmt::print("Z_NEED_DICT\n");
+                fmt::print(stderr, "Z_NEED_DICT\n");
                 return false;
             case Z_NEED_DICT:
-                fmt::print("Z_NEED_DICT\n");
+                fmt::print(stderr, "Z_NEED_DICT\n");
                 return false;
             case Z_DATA_ERROR:
-                fmt::print("Z_DATA_ERROR\n");
+                fmt::print(stderr, "Z_DATA_ERROR\n");
                 return false;
             case Z_MEM_ERROR:
-                fmt::print("Z_MEM_ERROR\n");
+                fmt::print(stderr, "Z_MEM_ERROR\n");
                 (void)inflateEnd(&zstrm);
                 return false;
         }
@@ -136,7 +137,6 @@ parser::parse_gz() noexcept
         ++stats_.bytes_shifted += buf.shift();
 
     } while (ret != Z_STREAM_END);
-    fmt::print("parse(): Z_STREAM_END, total_bytes_inflated={}\n", total_bytes_inflated);
 
     return true;
 }
@@ -160,27 +160,28 @@ parser::parse_itch(std::uint8_t const* buf, std::size_t bytes_to_read) noexcept
 
         // clang-format off
         // switch (hdr->message_type) {
-        //     case 'S': fmt::print(stderr, "{}\n", *reinterpret_cast<system_event const*>(hdr)); break;
-        //     case 'R': fmt::print(stderr, "{}\n", *reinterpret_cast<stock_directory const*>(hdr)); break;
-        //     case 'H': fmt::print(stderr, "{}\n", *reinterpret_cast<stock_trading_action const*>(hdr)); break;
-        //     case 'Y': fmt::print(stderr, "{}\n", *reinterpret_cast<reg_sho_restriction const*>(hdr)); break;
-        //     case 'L': fmt::print(stderr, "{}\n", *reinterpret_cast<market_participant_position const*>(hdr)); break;
-        //     case 'V': fmt::print(stderr, "{}\n", *reinterpret_cast<mwcb_decline_level const*>(hdr)); break;
-        //     case 'W': fmt::print(stderr, "{}\n", *reinterpret_cast<mwcb_status const*>(hdr)); break;
-        //     case 'K': fmt::print(stderr, "{}\n", *reinterpret_cast<ipo_quoting_period_update const*>(hdr)); break;
-        //     case 'J': fmt::print(stderr, "{}\n", *reinterpret_cast<luld_auction_collar const*>(hdr)); break;
-        //     case 'h': fmt::print(stderr, "{}\n", *reinterpret_cast<operational_halt const*>(hdr)); break;
+        //     case 'D': fmt::print(stderr, "{}\n", *reinterpret_cast<order_delete const*>(hdr)); break;
         //     case 'A': fmt::print(stderr, "{}\n", *reinterpret_cast<add_order const*>(hdr)); break;
+        //     case 'U': fmt::print(stderr, "{}\n", *reinterpret_cast<order_replace const*>(hdr)); break;
+        //     case 'X': fmt::print(stderr, "{}\n", *reinterpret_cast<order_cancel const*>(hdr)); break;
+        //     case 'I': fmt::print(stderr, "{}\n", *reinterpret_cast<noii const*>(hdr)); break;
         //     case 'F': fmt::print(stderr, "{}\n", *reinterpret_cast<add_order_with_mpid const*>(hdr)); break;
         //     case 'E': fmt::print(stderr, "{}\n", *reinterpret_cast<order_executed const*>(hdr)); break;
-        //     case 'C': fmt::print(stderr, "{}\n", *reinterpret_cast<order_executed_with_price const*>(hdr)); break;
-        //     case 'X': fmt::print(stderr, "{}\n", *reinterpret_cast<order_cancel const*>(hdr)); break;
-        //     case 'D': fmt::print(stderr, "{}\n", *reinterpret_cast<order_delete const*>(hdr)); break;
-        //     case 'U': fmt::print(stderr, "{}\n", *reinterpret_cast<order_replace const*>(hdr)); break;
+        //     case 'L': fmt::print(stderr, "{}\n", *reinterpret_cast<market_participant_position const*>(hdr)); break;
         //     case 'P': fmt::print(stderr, "{}\n", *reinterpret_cast<trade_non_cross const*>(hdr)); break;
+        //     case 'C': fmt::print(stderr, "{}\n", *reinterpret_cast<order_executed_with_price const*>(hdr)); break;
+        //     case 'Y': fmt::print(stderr, "{}\n", *reinterpret_cast<reg_sho_restriction const*>(hdr)); break;
+        //     case 'H': fmt::print(stderr, "{}\n", *reinterpret_cast<stock_trading_action const*>(hdr)); break;
         //     case 'Q': fmt::print(stderr, "{}\n", *reinterpret_cast<trade_cross const*>(hdr)); break;
+        //     case 'R': fmt::print(stderr, "{}\n", *reinterpret_cast<stock_directory const*>(hdr)); break;
+        //     case 'S': fmt::print(stderr, "{}\n", *reinterpret_cast<system_event const*>(hdr)); break;
+        //     case 'K': fmt::print(stderr, "{}\n", *reinterpret_cast<ipo_quoting_period_update const*>(hdr)); break;
+        //     case 'V': fmt::print(stderr, "{}\n", *reinterpret_cast<mwcb_decline_level const*>(hdr)); break;
+        //
+        //     case 'W': fmt::print(stderr, "{}\n", *reinterpret_cast<mwcb_status const*>(hdr)); break;
+        //     case 'J': fmt::print(stderr, "{}\n", *reinterpret_cast<luld_auction_collar const*>(hdr)); break;
+        //     case 'h': fmt::print(stderr, "{}\n", *reinterpret_cast<operational_halt const*>(hdr)); break;
         //     case 'B': fmt::print(stderr, "{}\n", *reinterpret_cast<broken_trade const*>(hdr)); break;
-        //     case 'I': fmt::print(stderr, "{}\n", *reinterpret_cast<noii const*>(hdr)); break;
 
         //     default: fmt::print("parse_itch(): unknown type=[{:c}]\n", hdr->message_type); std::exit(1); break;
         // }
