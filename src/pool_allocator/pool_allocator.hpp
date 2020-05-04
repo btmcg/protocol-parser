d5a6a87739c134f2ca9eb1d43dd6facfa72f8db7
brian <brian@eolas>
2020-05-04 12:29:47 -0400

    [pool_allocator] initial tests

--- src/pool_allocator/pool_allocator.hpp
+++ src/pool_allocator/pool_allocator.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <fmt/format.h>
-#include <cstddef>
+#include <cstddef> // std::byte
+#include <cstdint>
 #include <cstdlib>
 #include <utility> // std::forward
 
 
-template <typename T>
+template <typename T, std::size_t NumElements = 10>
 class pool_allocator
 {
 public:
@@ -14,11 +15,17 @@ public:
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
 
+    template <typename U>
+    struct rebind
+    {
+        using other = pool_allocator<T>;
+    };
+
 public:
     constexpr pool_allocator() noexcept;
-    constexpr pool_allocator(pool_allocator const&) noexcept;
-    template <typename U>
-    constexpr pool_allocator(pool_allocator<U> const&) noexcept;
+    constexpr pool_allocator(pool_allocator const&) noexcept = delete;
+    template <typename U, std::size_t NumU>
+    constexpr pool_allocator(pool_allocator<U, NumU> const&) noexcept = delete;
     ~pool_allocator() noexcept;
     [[nodiscard]] constexpr value_type* allocate(size_type) noexcept;
     constexpr void deallocate(value_type*, size_type);
@@ -26,67 +33,179 @@ public:
     void construct(U*, Args&&...);
     template <typename U>
     void destroy(U*);
+    constexpr std::size_t capacity() const noexcept;
+
+private:
+    void allocate_block();
+
+private:
+    union node
+    {
+        value_type element;
+        node* next;
+    };
+
+    std::byte* block_begin_;
+    std::byte* block_end_;
+    node* curr_node_;
+    node* last_node_;
 };
 
 
-template <typename T, typename U>
-constexpr bool operator==(pool_allocator<T> const&, pool_allocator<U> const&) noexcept;
+template <typename T, std::size_t NumElements, typename U, std::size_t NumU>
+constexpr bool operator==(
+        pool_allocator<T, NumElements> const&, pool_allocator<U, NumU> const&) noexcept;
+
+template <typename T, std::size_t NumElements, typename U, std::size_t NumU>
+constexpr bool operator!=(
+        pool_allocator<T, NumElements> const&, pool_allocator<U, NumU> const&) noexcept;
 
 /**********************************************************************/
 
-template <typename T>
-constexpr pool_allocator<T>::pool_allocator() noexcept
-{}
-
-template <typename T>
-constexpr pool_allocator<T>::pool_allocator(pool_allocator const&) noexcept
-{}
-
-template <typename T>
-template <typename U>
-constexpr pool_allocator<T>::pool_allocator(pool_allocator<U> const&) noexcept
-{}
-
-template <typename T>
-pool_allocator<T>::~pool_allocator() noexcept
-{}
-
-template <typename T>
-constexpr typename pool_allocator<T>::value_type*
-pool_allocator<T>::allocate(pool_allocator<T>::size_type n) noexcept
+template <typename T, std::size_t NumElements>
+constexpr pool_allocator<T, NumElements>::pool_allocator() noexcept
+        : block_begin_(nullptr)
+        , block_end_(nullptr)
+        , curr_node_(nullptr)
+        , last_node_(nullptr)
 {
-    // fmt::print(stderr, "malloc({})\n", n * sizeof(T));
-    return static_cast<value_type*>(::malloc(n * sizeof(T)));
+    fmt::print("pool_allocator::{}()\n", __func__);
+    allocate_block();
 }
 
-template <typename T>
+// template <typename T, std::size_t NumElements>
+// constexpr pool_allocator<T, NumElements>::pool_allocator(pool_allocator const& rhs) noexcept
+//         : block_begin_(rhs.block_begin_)
+//         , block_end_(rhs.block_end_)
+//         , curr_node_(rhs.curr_node_)
+//         , last_node_(rhs.last_node_)
+// {
+//     fmt::print("pool_allocator::{}()\n", __func__);
+//     // empty
+// }
+
+// template <typename T, std::size_t NumElements>
+// template <typename U, std::size_t NumU>
+// constexpr pool_allocator<T, NumElements>::pool_allocator(
+//         pool_allocator<U, NumU> const& rhs) noexcept
+//         : block_begin_(rhs.block_begin_)
+//         , block_end_(rhs.block_end_)
+//         , curr_node_(rhs.curr_node_)
+//         , last_node_(rhs.last_node_)
+// {
+//     fmt::print("pool_allocator::{}()\n", __func__);
+//     // empty
+// }
+
+template <typename T, std::size_t NumElements>
+pool_allocator<T, NumElements>::~pool_allocator() noexcept
+{
+    fmt::print("pool_allocator::{}()\n", __func__);
+    operator delete(block_begin_);
+    block_begin_ = nullptr;
+}
+
+template <typename T, std::size_t NumElements>
+constexpr typename pool_allocator<T, NumElements>::value_type*
+        pool_allocator<T, NumElements>::allocate(pool_allocator<T, NumElements>::size_type) noexcept
+{
+    fmt::print("pool_allocator::{}()\n", __func__);
+
+    if (curr_node_ == nullptr) {
+        fmt::print(stderr, "ERROR: pool_allocator::{}() out of space\n", __func__);
+        return nullptr;
+    }
+
+    value_type* ptr = (&curr_node_->element);
+    curr_node_ = curr_node_->next;
+
+    fmt::print(
+            "pool_allocator::{}() ptr={} curr_node_={}\n", __func__, (void*)ptr, (void*)curr_node_);
+    return ptr;
+}
+
+template <typename T, std::size_t NumElements>
 constexpr void
-pool_allocator<T>::deallocate(pool_allocator<T>::value_type* ptr, pool_allocator<T>::size_type)
+pool_allocator<T, NumElements>::deallocate(
+        pool_allocator<T, NumElements>::value_type* ptr, pool_allocator<T, NumElements>::size_type)
 {
-    ::free(ptr);
+    fmt::print("pool_allocator::{}()\n", __func__);
+    if (ptr == nullptr)
+        return;
+    reinterpret_cast<node*>(ptr)->next = curr_node_;
+    curr_node_ = reinterpret_cast<node*>(ptr);
 }
 
-template <typename T>
+template <typename T, std::size_t NumElements>
 template <typename U, typename... Args>
 void
-pool_allocator<T>::construct(U* ptr, Args&&... args)
+pool_allocator<T, NumElements>::construct(U* ptr, Args&&... args)
 {
+    fmt::print("pool_allocator::{}()\n", __func__);
     ::new (ptr) U(std::forward<Args>(args)...);
 }
 
-template <typename T>
+template <typename T, std::size_t NumElements>
 template <typename U>
 void
-pool_allocator<T>::destroy(U* ptr)
+pool_allocator<T, NumElements>::destroy(U* ptr)
 {
+    fmt::print("pool_allocator::{}()\n", __func__);
     ptr->~U();
+}
+
+template <typename T, std::size_t NumElements>
+constexpr std::size_t
+pool_allocator<T, NumElements>::capacity() const noexcept
+{
+    return std::distance(block_begin_, block_end_);
+}
+
+template <typename T, std::size_t NumElements>
+void
+pool_allocator<T, NumElements>::allocate_block()
+{
+    fmt::print("pool_allocator::{}()\n", __func__);
+    if (block_begin_ != nullptr) {
+        fmt::print(stderr, "pool_allocator::{}() block_begin_ != nullptr\n", __func__);
+        std::exit(1);
+    }
+
+    fmt::print("pool_allocator::{}() node size = {} bytes\n", __func__, sizeof(node));
+    fmt::print("pool_allocator::{}() allocating {} bytes\n", __func__, NumElements * sizeof(node));
+    block_begin_
+            = reinterpret_cast<decltype(block_begin_)>(operator new(NumElements * sizeof(node)));
+    block_end_ = block_begin_ + (NumElements * sizeof(node));
+    curr_node_ = reinterpret_cast<decltype(curr_node_)>(block_begin_);
+
+    fmt::print("pool_allocator::{}() block_begin_={}\n", __func__, (void*)block_begin_);
+    fmt::print("pool_allocator::{}() block_end_={}\n", __func__, (void*)block_end_);
+    fmt::print("pool_allocator::{}() curr_node_={}\n", __func__, (void*)curr_node_);
+    node* tmp = curr_node_;
+    for (std::size_t i = 1; i < NumElements; ++i) {
+        tmp->next = tmp + 1;
+        tmp = tmp->next;
+        fmt::print("pool_allocator::{}() node {}={}\n", __func__, i, (void*)tmp);
+    }
+    last_node_ = tmp;
+    last_node_->next = nullptr;
+    fmt::print("pool_allocator::{}() last_node_={}\n", __func__, (void*)last_node_);
 }
 
 /**********************************************************************/
 
-template <typename T, typename U>
+template <typename T, std::size_t NumElements, typename U, std::size_t NumU>
 constexpr bool
-operator==(pool_allocator<T> const&, pool_allocator<U> const&) noexcept
+operator==(pool_allocator<T, NumElements> const&, pool_allocator<U, NumU> const&) noexcept
 {
+    fmt::print("pool_allocator::{}()\n", __func__);
     return false;
+}
+
+template <typename T, std::size_t NumElements, typename U, std::size_t NumU>
+constexpr bool
+operator!=(pool_allocator<T, NumElements> const&, pool_allocator<U, NumU> const&) noexcept
+{
+    fmt::print("pool_allocator::{}()\n", __func__);
+    return true;
 }
--- test/test_pool_allocator.cpp
+++ test/test_pool_allocator.cpp
@@ -19,28 +19,104 @@ struct object
 
 TEST_CASE("pool allocator", "[pool_allocator]")
 {
-    // pool_allocator<int> alloc;
+    SECTION("allocator requirements")
+    {
+        pool_allocator<int> alloc1;
+        pool_allocator<int> alloc2;
 
-    // SECTION("basic")
+        REQUIRE(alloc1 != alloc2);
+        REQUIRE_FALSE(alloc1 == alloc2);
+
+        // pool_allocator<int> alloc3(alloc1);
+        // pool_allocator<int> alloc4 = alloc1;
+        // pool_allocator<int> alloc5(std::move(alloc1));
+        // pool_allocator<int> alloc6 = std::move(alloc1);
+    }
+
+    SECTION("allocate and deallocate")
+    {
+        pool_allocator<int> alloc;
+        REQUIRE(alloc.capacity() == sizeof(int*) * 10);
+
+        // ensure array starts with all nullptrs
+        int* ptrs[10] = {};
+        for (int i = 0; i < 10; ++i)
+            REQUIRE(ptrs[i] == nullptr);
+
+        // set every array element to it's index value
+        for (int i = 0; i < 10; ++i) {
+            ptrs[i] = alloc.allocate(1);
+            *ptrs[i] = i;
+        }
+
+        // ensure array contains 0,1,2,...,9
+        for (int i = 0; i < 10; ++i)
+            REQUIRE(*ptrs[i] == i);
+
+        // deallocate 5th ptr and reallocate and set to 500
+        alloc.deallocate(ptrs[5], 1);
+        int* ptr = alloc.allocate(1);
+        *ptr = 500;
+
+        for (int i = 0; i < 10; ++i) {
+            if (i == 5) {
+                REQUIRE(*ptrs[i] == i * 100);
+            } else {
+                REQUIRE(*ptrs[i] == i);
+            }
+        }
+
+        // deallocate 9th ptr and reallocate and set to 900
+        alloc.deallocate(ptrs[9], 1);
+        ptr = alloc.allocate(1);
+        *ptr = 900;
+
+        for (int i = 0; i < 10; ++i) {
+            if (i == 5 || i == 9) {
+                REQUIRE(*ptrs[i] == i * 100);
+            } else {
+                REQUIRE(*ptrs[i] == i);
+            }
+        }
+
+        alloc.deallocate(ptrs[0], 1);
+        alloc.deallocate(ptrs[2], 1);
+        alloc.deallocate(ptrs[4], 1);
+
+        ptr = alloc.allocate(1);
+        *ptr = 400;
+        ptr = alloc.allocate(1);
+        *ptr = 200;
+        ptr = alloc.allocate(1);
+        *ptr = 0;
+
+        for (int i = 0; i < 10; ++i) {
+            if (i == 0 || i == 2 || i == 4 || i == 5 || i == 9) {
+                REQUIRE(*ptrs[i] == i * 100);
+            } else {
+                REQUIRE(*ptrs[i] == i);
+            }
+        }
+
+        // attempt to allocate when no mem left
+        REQUIRE(alloc.allocate(1) == nullptr);
+    }
+
+    // SECTION("int list")
     // {
-    //     int* ptr = alloc.allocate(sizeof(int));
-    //     *ptr = 42;
-    //     REQUIRE(*ptr == 42);
-
-    //     alloc.deallocate(ptr, sizeof(int));
+    //     std::list<int, pool_allocator<int, 10>> intlist;
+    //     intlist.push_back(42);
+    //     intlist.push_back(43);
+    //     intlist.push_back(44);
+    //     intlist.push_back(45);
     // }
 
-    SECTION("replacement for std::allocator")
-    {
-        std::list<object, pool_allocator<object>> objlist;
-        objlist.push_back(object{1, 2, 3});
-        objlist.push_back(object{4, 5, 6});
-        objlist.push_back(object{7, 8, 9});
-
-        std::list<int, pool_allocator<int>> intlist;
-        intlist.push_back(42);
-        intlist.push_back(43);
-        intlist.push_back(44);
-        intlist.push_back(45);
-    }
+    // SECTION("custom object in list")
+    // {
+    //     REQUIRE(sizeof(object) == 12);
+    //     std::list<object, pool_allocator<object, 10>> objlist;
+    //     objlist.push_back(object{1, 2, 3});
+    //     objlist.push_back(object{4, 5, 6});
+    //     objlist.push_back(object{7, 8, 9});
+    // }
 }
