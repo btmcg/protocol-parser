93e0eaa985f107798b1486e4748592c66d4983f5
brian <brian@eolas>
2020-05-05 11:26:33 -0400

    [assert] use func, file, line builtins

--- src/common/assert.hpp
+++ src/common/assert.hpp
@@ -4,14 +4,11 @@
 #ifdef BTMCG_DEBUG
 #    define DEBUG_ASSERT(expr) \
         do { \
-            static_cast<void>((expr) \
-                    || (handle_failed_debug_assertion( \
-                                "\"" #expr "\"", __func__, __FILE__, __LINE__), \
-                            true)); \
+            static_cast<void>((expr) || (handle_failed_debug_assertion("\"" #expr "\""), true)); \
         } while (0)
 #else
 #    define DEBUG_ASSERT(expr)
 #endif
 
-void handle_failed_debug_assertion(
-        char const* msg, char const* func, char const* file, int line) noexcept;
+void handle_failed_debug_assertion(char const* msg, char const* func = __builtin_FUNCTION(),
+        char const* file = __builtin_FILE(), int line = __builtin_LINE()) noexcept;
