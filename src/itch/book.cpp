9b94bafb27ea927791f67ccd3fbe064b8b48b100
brian <brian@eolas>
2020-05-26 16:58:25 -0400

    [itch book] decrease bid/ask pool size

--- src/itch/book.cpp
+++ src/itch/book.cpp
@@ -7,9 +7,9 @@
 namespace itch {
 
     tsbook::tsbook() noexcept
-            : bid_pool_(sizeof(price_level) + 16, 10000)
+            : bid_pool_(sizeof(price_level) + 16, 7500)
             , bids_(bid_pool_)
-            , ask_pool_(sizeof(price_level) + 16, 10000)
+            , ask_pool_(sizeof(price_level) + 16, 7500)
             , asks_(ask_pool_)
     {
         // empty
