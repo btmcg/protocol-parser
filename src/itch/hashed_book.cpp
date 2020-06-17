a4d28568e0d9670911a494e8aaaebdf6a20b7a4d
brian <brian@eolas>
2020-06-17 19:48:40 -0400

    [itch::hashed_book] rename order param

--- src/itch/hashed_book.cpp
+++ src/itch/hashed_book.cpp
@@ -37,36 +37,36 @@ namespace itch {
     }
 
     void
-    hashed_book::add_order(order& o) noexcept
+    hashed_book::add_order(order& order) noexcept
     {
-        auto* book = (o.side == Side::Bid) ? &bids_ : &asks_;
-        auto* map = (o.side == Side::Bid) ? &bid_map_ : &ask_map_;
+        auto* book = (order.side == Side::Bid) ? &bids_ : &asks_;
+        auto* map = (order.side == Side::Bid) ? &bid_map_ : &ask_map_;
 
         // need to catch any exceptions from std::unordered_map::emplace
         try {
             if (book->empty()) {
-                auto new_itr = book->emplace(book->begin(), o.price, o.qty);
-                map->emplace(o.price, new_itr);
-                o.pl = &(*new_itr);
+                auto new_itr = book->emplace(book->begin(), order.price, order.qty);
+                map->emplace(order.price, new_itr);
+                order.pl = &(*new_itr);
             } else {
-                auto map_itr = map->find(o.price);
+                auto map_itr = map->find(order.price);
 
                 // price not found in the map, therefore it doesn't exist in the
                 // book. find out where it should go
                 if (map_itr == map->end()) {
                     // find location in book
-                    auto loc = std::find_if(book->begin(), book->end(), [&o](auto const& pl) {
-                        return (o.side == Side::Bid) ? pl.price() <= o.price
-                                                     : pl.price() >= o.price;
+                    auto loc = std::find_if(book->begin(), book->end(), [&order](auto const& pl) {
+                        return (order.side == Side::Bid) ? pl.price() <= order.price
+                                                         : pl.price() >= order.price;
                     });
 
                     // new price level
-                    auto new_itr = book->emplace(loc, o.price, o.qty);
-                    map->emplace(o.price, new_itr);
-                    o.pl = &(*new_itr);
+                    auto new_itr = book->emplace(loc, order.price, order.qty);
+                    map->emplace(order.price, new_itr);
+                    order.pl = &(*new_itr);
                 } else {
-                    map_itr->second->inc_qty(o.qty);
-                    o.pl = &*(map_itr->second);
+                    map_itr->second->inc_qty(order.qty);
+                    order.pl = &*(map_itr->second);
                 }
             }
         } catch (std::exception const& e) {
