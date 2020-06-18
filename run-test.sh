39f6bba65be8430bd94133933b6e4e6ca57f4384
brian <brian@eolas>
2020-06-18 10:27:42 -0400

    [run-test] write version string once

--- run-test.sh
+++ run-test.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 data_file="${HOME}/var/downloads/itch/01302020.NASDAQ_ITCH50"
+output="./runtimes"
 
+bin/parser -v >> "${output}"
 for run in 0 1 2; do
-    bin/parser "${data_file}" >> runtimes && bin/parser -v >> runtimes
+    bin/parser "${data_file}" >> "${output}"
 done
