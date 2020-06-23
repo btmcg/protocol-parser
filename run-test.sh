63ee74bce7009cd078329de37d4e7aa2d343fd84
brian <brian@logos>
2020-06-23 11:38:12 -0400

    [run-test] more info written to disk and stdout

--- run-test.sh
+++ run-test.sh
@@ -3,7 +3,10 @@
 data_file="tmp/itch-data/01302020.NASDAQ_ITCH50"
 output="./runtimes"
 
+printf "# %s %s %s\n" "$(uname --nodename)" "$(uname --kernel-release)" "${data_file}" >> "${output}"
 bin/parser -v >> "${output}"
 for run in 0 1 2; do
+    printf "run %s\n" "${run}"
     bin/parser "${data_file}" >> "${output}"
+    printf "\n" >> "${output}"
 done
