fe11a74938e3d3ddf9057ebbd592b68a4fa02dbe
brian <brian@eolas>
2020-08-21 18:45:48 -0400

    [test] add protocol as dependency

--- test/Module.mk
+++ test/Module.mk
@@ -1,7 +1,7 @@
 MODULE_NAME := test-runner
 MODULE_CPPFLAGS := -I.
 MODULE_SOURCE_FILES := $(call rwildcard,$(get-path),*.cpp)
-MODULE_LIBRARIES := allocator itch util
+MODULE_LIBRARIES := allocator itch protocol util
 
 $(use-catch)
 $(use-fmt)
